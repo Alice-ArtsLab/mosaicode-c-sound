7279c46acbf0efe693116311bba716e8869470c0
llgoncalves <luanlg.cco@gmail.com>
2017-12-01 20:19:28 -0200

    Created sound port, code template, speaker and white noise

---
+++ setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+from glob import glob
+
+DISTUTILS_DEBUG = "True"
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+config = {}
+
+config['classifiers'] = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: GNU General Public License (GPL)',
+    'Natural Language :: English',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: C',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Software Development :: Code Generators',
+]
+
+setup(name='mosaicode_lib_c_sound',
+      install_requires=['Python>=2.7'],
+      tests_require=[],
+      test_suite='',
+      version='1.0',
+      packages = find_packages(exclude=["tests.*", "tests"]),
+      scripts=[],
+      description='Computer Music Programming Tool',
+      author='Mosaicode Group',
+      author_email='mosaicode-dev@googlegroups.com',
+      maintainer="Mosaicode Group",
+      maintainer_email="mosaicode-dev@googlegroups.com",
+      license="GNU GPL3",
+      url='https://mosaicode.github.io/',
+
+      # this is fucked up! must put it in package_data!!
+      data_files=[],
+      **config
+      )
